615d6cf31e836b1b8a7ef73f534ce4425c864b0f
JoshuaDalida <141461365+JoshuaDalida@users.noreply.github.com>
2024-01-16 19:07:16 -0700

    1.1

---
+++ app/Week 2/Student-info.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      <main>
+        <h1>Joshua Dalida</h1>
+        <a href = "https://github.com/JoshuaDalida">A link to my github!</a>
+      </main>
+    );
+  }
---
+++ app/Week 2/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <h1>Joshua Dalida</h1>
+        <a href = "https://github.com/JoshuaDalida">A link to my github!</a>
+      </main>
+    );
+  }
