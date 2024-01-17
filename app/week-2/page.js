7fc0d682fac166b79407429de1e4e397f100c490
JoshuaDalida <141461365+JoshuaDalida@users.noreply.github.com>
2024-01-16 20:06:46 -0700

    week 2 assignment 1.2

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,11 +1,10 @@
-import './globals.css'
 
 export default function Page() {
-    return (
-      <main className="flex min-h-screen flex-col">
-        <h1>Shopping List</h1>
-        <h1>Joshua Dalida</h1>
-        <a href = "https://github.com/JoshuaDalida" className ="hover:text-blue-400 hover:underline">A link to my github!</a>
-      </main>
-    );
-  }
+  return (
+    <main className="flex min-h-screen flex-col">
+      <h1>Shopping List</h1>
+      <h1>Joshua Dalida</h1>
+      <a href="https://github.com/JoshuaDalida" className="hover:text-blue-400 hover:underline">A link to my github!</a>
+    </main>
+  );
+}
