95af6e58d7ad8afe8a4457efbb707830704dd46c
JoshuaDalida <141461365+JoshuaDalida@users.noreply.github.com>
2024-01-16 19:55:44 -0700

    week 2 assignment 1.1

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className ="text-900">CPRG 306: Web Development 2 - Assignments</h1>
       <h1>
-      <Link href="./week-2">Week 2</Link>
+      <Link href="./week-2" className="hover:text-blue-400 hover:underline">Week 2</Link>
       </h1>
     </main>
   );
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,11 @@
+import './globals.css'
+
 export default function Page() {
     return (
-      <main>
+      <main className="flex min-h-screen flex-col">
         <h1>Shopping List</h1>
         <h1>Joshua Dalida</h1>
-        <a href = "https://github.com/JoshuaDalida">A link to my github!</a>
+        <a href = "https://github.com/JoshuaDalida" className ="hover:text-blue-400 hover:underline">A link to my github!</a>
       </main>
     );
   }
